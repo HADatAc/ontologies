b2750aae4d16d1626daf0a23cba1fb78e1e1962e
Henrique Santos <oliveh@rpi.edu>
2023-10-10 09:41:38 -0400

    ls

--- .hygiene/run_hygiene.sh
+++ .hygiene/run_hygiene.sh
@@ -25,6 +25,8 @@ docker exec hygiene /publisher/publish.sh hygiene
 # Copy results to the local output dir
 docker cp hygiene:"/output/$ONTPUB_FAMILY" "$OUTPUT_DIR"
 
+ls -l "$OUTPUT_DIR"
+
 docker-compose down
 
 npx verify-junit-xml "$OUTPUT_DIR/$ONTPUB_FAMILY/ontology/"*"/latest/hygiene_test.dev.xml"
